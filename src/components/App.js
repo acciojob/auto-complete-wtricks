39b7f261430745f6d1c7297465a4cb921a01a22f
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-10-31 21:24:22 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,36 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [list, setList] = React.useState([
+    "apple", 
+    "banana", 
+    "cherry", 
+    "date", 
+    "elderberry", 
+    "fig"
+  ])
+
+  const input = React.useRef()
+  const ref = React.useRef()
+  const [ans, setAns] = React.useState(list)
+
+  const onChange = () => {
+    clearTimeout(ref.current)
+    ref.current = setTimeout(() => {
+      const regex = new RegExp('^' + input.current.value)
+      setAns(p => p.filter(t => regex.test(t)))
+    }, 10)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h2>Search item</h2>
+        <input ref={input} onChange={onChange} type="text" placeholder="Search" name="input" />
+
+        <ul>
+          {ans.map((item, index) => (<li key={index}>{item}</li>))}
+        </ul>
     </div>
   )
 }
